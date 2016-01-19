829b5fc31c275c222452f860a9e3215c049b15fd
kanapyshka <kanapysh@gmail.com>
2016-01-19 17:16:08 +0200

    Create lesson_001.java

---
+++ lesson_001.java
@@ -0,0 +1,25 @@
+package lesson_001;
+
+import java.lang.String;
+
+public class Variables {
+    // Instance Variables (Non-Static Fields)
+    int testField;                  // Field of Integer without initialization
+    int newTestField = 0;           // Field of Integer without initialization
+
+    // Class Variables (Static Fields)
+    static int staticField;         // Field of Integer without initialization
+
+    public void practice()
+    {
+        // Local Variables
+        int width = 0;                  // Variable of Integer without initialization
+        int height = 15;            // Variable of Integer with initialization
+        String name = "test";       // Variable of String without initialization
+        boolean status = true;
+    }
+
+    private void testMethod(int testParameter) // Parameter variable
+    {
+    }
+}
